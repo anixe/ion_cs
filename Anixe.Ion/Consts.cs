206055eac2eca95d471feed1affbb34de1546891
Karol Miszczyk <karol.miszczyk@anixe.pl>
2016-09-01 15:00:04 +0200

    IonReaderFactory - creating instance of IonReader basing on file path or stream

---
+++ Anixe.Ion/Consts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Anixe.Ion
+{
+    public static class Consts
+    {
+        public static class ErrorMessages
+        {
+            public const string UndefinedFilePath = "File path must be defined!";
+            public const string FileDoesNotExist  = "File '{0}' does not exist!";
+        }
+    }
+}
+
